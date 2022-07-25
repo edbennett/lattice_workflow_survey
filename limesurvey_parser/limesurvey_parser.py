7d0f5a49edde3479597bce5fd434e3ef760f30c3
Julian Lenz <j.j.lenz@swansea.ac.uk>
2022-07-25 15:54:53 +0100

    Splits headers

--- limesurvey_parser/limesurvey_parser.py
+++ limesurvey_parser/limesurvey_parser.py
@@ -6,15 +6,29 @@ import parse
 
 
 class LimeSurveyParser:
-    sep: str
+    sep_csv: str
+    sep_header: str
 
-    def __init__(self, sep: str = ";") -> None:
-        self.sep = sep
+    def __init__(self, sep_csv: str = ";", sep_header: str = "---") -> None:
+        self.sep_csv = sep_csv
+        self.sep_header = sep_header
 
     def parse(self, content: str) -> pd.DataFrame:
         if not content:
             return pd.DataFrame()
-        return pd.read_csv(StringIO(content), header=0, sep=self.sep)
+        original_data = pd.read_csv(StringIO(content), header=0, sep=self.sep_csv)
+        return self._organize_header(original_data)
+
+    def _organize_header(self, data: pd.DataFrame) -> pd.DataFrame:
+        new_columns = [
+            tup if len(tup) == 2 else (None, tup[0])
+            for tup in (entry.split(self.sep_header) for entry in data.columns)
+        ]
+        data.columns = pd.MultiIndex.from_tuples(
+            new_columns,
+            names=["id", "title"],
+        )
+        return data
 
     def parse_question_id(self, id_string: str) -> Dict[str, int]:
         # The extra `dict` is a dirty hack for mypy. Should better use a stub
--- test/test_limesurvey_parser.py
+++ test/test_limesurvey_parser.py
@@ -1,3 +1,5 @@
+from typing import Iterable
+
 import numpy as np
 import pandas as pd
 import pytest
@@ -10,12 +12,18 @@ def parser() -> LimeSurveyParser:
     return LimeSurveyParser()
 
 
+def make_columns_structure_from(iterable: Iterable[str]) -> pd.MultiIndex:
+    return pd.MultiIndex.from_tuples(
+        [(None, val) for val in iterable], names=["id", "title"]
+    )
+
+
 def test_returns_data_frame_on_empty_input(parser: LimeSurveyParser) -> None:
     assert type(parser.parse("")) == pd.DataFrame
 
 
 def test_parses_first_line_as_header(parser: LimeSurveyParser) -> None:
-    assert parser.parse("header").columns == pd.Index(["header"])
+    assert parser.parse("header").columns == make_columns_structure_from(["header"])
 
 
 def test_parses_second_line_as_data(parser: LimeSurveyParser) -> None:
@@ -26,7 +34,10 @@ def test_uses_semicolon_as_default_separator(parser: LimeSurveyParser) -> None:
     assert (
         (
             parser.parse("header1;header2\ndata1;data2")
-            == pd.DataFrame([["data1", "data2"]], columns=["header1", "header2"])
+            == pd.DataFrame(
+                [["data1", "data2"]],
+                columns=make_columns_structure_from(["header1", "header2"]),
+            )
         )
         # a bit weird but first creates a pd.Series, second makes a bool:
         .all().all()
@@ -34,11 +45,14 @@ def test_uses_semicolon_as_default_separator(parser: LimeSurveyParser) -> None:
 
 
 def test_separator_can_be_configured() -> None:
-    parser = LimeSurveyParser(sep=",")
+    parser = LimeSurveyParser(sep_csv=",")
     assert (
         (
             parser.parse("header1,header2\ndata1,data2")
-            == pd.DataFrame([["data1", "data2"]], columns=["header1", "header2"])
+            == pd.DataFrame(
+                [["data1", "data2"]],
+                columns=make_columns_structure_from(["header1", "header2"]),
+            )
         )
         # a bit weird but first creates a pd.Series, second makes a bool:
         .all().all()
@@ -52,4 +66,13 @@ def test_parses_question_id(parser: LimeSurveyParser) -> None:
 def test_parses_question_id_with_selected_answer(parser: LimeSurveyParser) -> None:
     assert parser.parse_question_id("G01Q2[SQ003]") == dict(
         group=1, question=2, answer=3
+    )
+
+
+def test_splits_headers_with_separator_in_them(parser: LimeSurveyParser) -> None:
+    default_separator = "---"
+    assert parser.parse(
+        "head_id" + default_separator + "head_title"
+    ).columns == pd.MultiIndex.from_tuples(
+        [("head_id", "head_title")], names=["id", "title"]
     )
