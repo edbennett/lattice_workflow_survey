d8b197cbb98014f3c6460e4c0bab4a6b4e4e09a4
Julian Lenz <j.j.lenz@swansea.ac.uk>
2022-07-25 12:30:51 +0100

    Added parser fixture

--- test/test_limesurvey_parser.py
+++ test/test_limesurvey_parser.py
@@ -1,12 +1,16 @@
+import pytest
 from limesurvey_parser import LimeSurveyParser
 import pandas as pd
 
 
-def test_returns_data_frame_on_empty_input() -> None:
-    parser = LimeSurveyParser()
+@pytest.fixture
+def parser() -> LimeSurveyParser:
+    return LimeSurveyParser()
+
+
+def test_returns_data_frame_on_empty_input(parser) -> None:
     assert type(parser.parse("")) == pd.DataFrame
 
 
-def test_parses_first_line_as_header() -> None:
-    parser = LimeSurveyParser()
+def test_parses_first_line_as_header(parser) -> None:
     assert parser.parse("header").columns == pd.Index(["header"])
