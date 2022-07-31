aa669489134d59646ac18527ffc9e0c2bdafb7bc
Ed Bennett <e.j.bennett@swansea.ac.uk>
2022-07-31 14:29:00 +0100

    use Julian's parser to analyse survey data

---
+++ src/redact.py
@@ -0,0 +1,31 @@
+from argparse import ArgumentParser
+import sys
+
+import pandas as pd
+
+
+def redact(df, question_ids):
+    for column in df.columns:
+        for question_id in question_ids:
+            if question_id in column and column in df.columns:
+                df = df.drop(columns=column)
+    return df
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("input_filename")
+    parser.add_argument("question_ids", nargs="+")
+    parser.add_argument("--output_filename", default=sys.stdout)
+    parser.add_argument("--delimiter", default=";")
+    args = parser.parse_args()
+
+    df = redact(
+        pd.read_csv(args.input_filename, delimiter=args.delimiter),
+        args.question_ids
+    )
+    df.to_csv(args.output_filename)
+
+
+if __name__ == "__main__":
+    main()
